67951fd92d4dda428cfbd73c407c96cae418ccea
Elegant Lai <elegant.lai@kiwifi.cn>
2015-09-07 21:58:02 +0800

    Signed-off-by: Elegant Lai <elegant.lai@kiwifi.cn>

--- modules/export=/LetterOnlyValidator.ts
+++ modules/export=/LetterOnlyValidator.ts
@@ -1,3 +1,8 @@
+import validation = require('./Validation');
+var letterReg = /^[A-Za-z]+$/;
+class LetterOnlyValidator implements validation.StringValidator {
     isAcceptable(s: string) {
+        return letterReg.test(s);
     }
 }
+export = LetterOnlyValidator;
